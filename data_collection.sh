4bce64d8347950f015dc6d5577790f43ccb308af
Pranav Sitaraman <sitaraman.pranav@gmail.com>
2024-02-23 19:26:54 -0500

    data collection

---
+++ data_collection.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+roslaunch ldlidar_sl_ros viewer_ld14p_noetic.launch
+rosbag record -O recording.bag pointcloud2d
+rosbag filter --print="'%d.%d, %d : %s' % (t.secs % 100, t.nsecs, len(m.points), [(m.points[i].x, m.points[i].y) for i in range(len(m.points)) if (m.points[i].x != 0 or m.points[i].y != 0)])" recording.bag filtered.bag "topic=='pointcloud2d'" > cleaned.txt
