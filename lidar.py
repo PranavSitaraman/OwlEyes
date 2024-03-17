7191da01aefea1dbe7db9c765e1ca66b9562cda0
Pranav Sitaraman <sitaraman.pranav@gmail.com>
2024-03-17 18:37:24 -0400

    lidar serial

--- lidar.py
+++ lidar.py
@@ -1,8 +1,20 @@
 import RPi.GPIO as GPIO
+import serial
 import time
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(18, GPIO.OUT)
 GPIO.output(18, GPIO.HIGH)
-print("Working")
-time.sleep(5)
+ser = serial.Serial(
+    port="/dev/ttyAMA0",
+    baudrate=230400,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=1,
+)
+start = time.time()
+while time.time() < start + 5:
+    read_byte = ser.read()
+    print("0x" + read_byte.hex() + ", ", end=" ")
+GPIO.output(18, GPIO.LOW)
 GPIO.cleanup()
