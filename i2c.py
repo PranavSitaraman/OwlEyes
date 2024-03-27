ab21d81097c43aa564b79ece7051e877fb198f5d
Pranav Sitaraman <sitaraman.pranav@gmail.com>
2024-03-26 21:51:03 -0400

    Update i2c.py

--- i2c.py
+++ i2c.py
@@ -1,30 +1,6 @@
-# from smbus2 import SMBus
-# import time
-# address = 0x28
-# bus = SMBus(1)
-# bus.write_i2c_block_data(address, 0, [0X3D, 0X00])
-# bus.write_i2c_block_data(address, 0, [0X3F, 0x20])
-# time.sleep(1)
-# bus.write_i2c_block_data(address, 0, [0X3E, 0X00])
-# time.sleep(0.01)
-# bus.write_i2c_block_data(address, 0, [0X07, 0])
-# bus.write_i2c_block_data(address, 0, [0X3F, 0])
-# time.sleep(0.01)
-# bus.write_i2c_block_data(address, 0, [0X3D, 0X0C])
-# time.sleep(0.02)
-
-# while True:
-#     bus.write_i2c_block_data(address, 0, [0X28])
-#     buffer = bus.read_i2c_block_data(address, 0, 6)
-#     x = ((buffer[0]) | ((buffer[1]) << 8)) / 100
-#     y = ((buffer[2]) | ((buffer[3]) << 8)) / 100
-#     z = ((buffer[4]) | ((buffer[5]) << 8)) / 100
-#     print(x, y, z)
-
-# bus.close()
-
 from adafruit_extended_bus import ExtendedI2C as I2C
 import time
+import math
 i2c = I2C(1)
 address = 0x28
 i2c.writeto(address, bytes([0X3D, 0X00]))
@@ -52,4 +28,20 @@ while True:
     x /= 100
     y /= 100
     z /= 100
-    print(x, y, z)
+    i2c.writeto_then_readfrom(address, bytes([0X1A]), buffer)
+    a = ((buffer[0]) | ((buffer[1]) << 8))
+    b = ((buffer[2]) | ((buffer[3]) << 8))
+    c = ((buffer[4]) | ((buffer[5]) << 8))
+    if a > 32767:
+        a -= 65536
+    if b > 32767:
+        b -= 65536
+    if c > 32767:
+        c -= 65536
+    a /= 16
+    b /= 16
+    c /= 16
+    vel_x = 0.01 * x/math.cos(math.pi/180 * a)
+    vel_y = 0.01 * y/math.cos(math.pi/180 * b)
+    print(vel_x, vel_y)
+    time.sleep(0.01)
