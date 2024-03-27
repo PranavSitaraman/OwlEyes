c0b1661c2782b4168bec80a4ef19eacefcc383e9
Ubuntu <sitaraman.pranav@gmail.com>
2024-03-27 01:39:24 +0000

    conversion

--- i2c.py
+++ i2c.py
@@ -27,20 +27,29 @@ from adafruit_extended_bus import ExtendedI2C as I2C
 import time
 i2c = I2C(1)
 address = 0x28
-i2c.writeto(address, [0X3D, 0X00])
-i2c.writeto(address, [0X3F, 0x20])
+i2c.writeto(address, bytes([0X3D, 0X00]))
+i2c.writeto(address, bytes([0X3F, 0x20]))
 time.sleep(1)
-i2c.writeto(address, [0X3E, 0X00])
-time.sleep(0.01)
-i2c.writeto(address, [0X07, 0])
-i2c.writeto(address, [0X3F, 0])
-time.sleep(0.01)
-i2c.writeto(address, [0X3D, 0X0C])
-time.sleep(0.02)
+i2c.writeto(address, bytes([0X3E, 0X00]))
+time.sleep(1)
+i2c.writeto(address, bytes([0X07, 0]))
+i2c.writeto(address, bytes([0X3F, 0]))
+time.sleep(1)
+i2c.writeto(address, bytes([0X3D, 0X0C]))
+time.sleep(1)
 while True:
     buffer = [0] * 6
-    i2c.writeto_then_readfrom(address, [0X28], buffer)
-    x = ((buffer[0]) | ((buffer[1]) << 8)) / 100
-    y = ((buffer[2]) | ((buffer[3]) << 8)) / 100
-    z = ((buffer[4]) | ((buffer[5]) << 8)) / 100
+    i2c.writeto_then_readfrom(address, bytes([0X28]), buffer)
+    x = ((buffer[0]) | ((buffer[1]) << 8))
+    y = ((buffer[2]) | ((buffer[3]) << 8))
+    z = ((buffer[4]) | ((buffer[5]) << 8))
+    if x > 32767:
+        x -= 65536
+    if y > 32767:
+        y -= 65536
+    if z > 32767:
+        z -= 65536
+    x /= 100
+    y /= 100
+    z /= 100
     print(x, y, z)
